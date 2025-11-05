cf7b399445e6988a963d0468f99cbef6f5fba530
Desire <desireirankundawilliams@gmail.com>
2025-11-05 22:54:27 +0200

    cleaned the code

--- src/auth/PersonInfo.js
+++ src/auth/PersonInfo.js
@@ -1,4 +1,11 @@
-import { Button, Checkbox, Group, TextInput, NumberInput } from "@mantine/core";
+import {
+  Button,
+  Checkbox,
+  Group,
+  TextInput,
+  NumberInput,
+  Container,
+} from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
 import { useState } from "react";
 import { IconCalendar } from "@tabler/icons-react";
@@ -6,65 +13,56 @@ import "@mantine/dates/styles.css";
 import { Radio } from "@mantine/core";
 
 const PersonInfo = ({ form }) => {
-  const [value, setValue] = useState(null); 
+  const [value, setValue] = useState(null);
 
   return (
-    <div>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput label="Full Name" placeholder="Samson Kwizela" required/>
+    <Container size={600}>
+      <TextInput
+        label="Full Name"
+        placeholder="Samson Kwizela"
+        required
+        size="md"
+      />
 
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="your@email.com"
-          key={form.key("email")}
-          {...form.getInputProps("email")}
-          required
-        />
+      <TextInput
+        withAsterisk
+        label="Email"
+        placeholder="your@email.com"
+        key={form.key("email")}
+        {...form.getInputProps("email")}
+        size="md"
+      />
 
-        <NumberInput label="Phone" placeholder="+260" />
+      <NumberInput label="Phone" placeholder="+260" size="md" />
 
-        <DatePickerInput
-          label="Date of Birth"
-          placeholder="Select your D.O.B"
-          icon={<IconCalendar size={18} />}
-          value={value}
-          onChange={setValue}
-          valueFormat="DD/MM/YYYY"
-          required
-          withAsterisk
-          maxDate={new Date()}
-        />
+      <DatePickerInput
+        label="Date of Birth"
+        placeholder="Select your D.O.B"
+        icon={<IconCalendar size={18} />}
+        value={value}
+        onChange={setValue}
+        valueFormat="DD/MM/YYYY"
+        required
+        withAsterisk
+        maxDate={new Date()}
+        size="md"
+      />
 
-        <TextInput label="Address" placeholder="Lusaka" required/>
+      <TextInput label="Address" placeholder="Lusaka" required size="md" />
 
-                <Radio.Group
-          name="gender"
-          label="Gender"
-          description="Select your gender"
-          withAsterisk
-        >
-          <Group mt="xs">
-            <Radio value="male" label="Male" />
-            <Radio value="female" label="Female" />
-            <Radio value="other" label="Prefer not to say" />
-          </Group>
-        </Radio.Group>
-
-        <Checkbox
-          mt="md"
-          label="I agree to sell my privacy"
-          key={form.key("termsOfService")}
-          {...form.getInputProps("termsOfService", { type: "checkbox" })}
-        />
-        
-
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">Submit</Button>
+      <Radio.Group
+        name="gender"
+        label="Gender"
+        description="Select your gender"
+        withAsterisk
+      >
+        <Group mt="xs">
+          <Radio value="male" label="Male" />
+          <Radio value="female" label="Female" />
+          <Radio value="other" label="Prefer not to say" />
         </Group>
-
-      </form>
-    </div>
+      </Radio.Group>
+    </Container>
   );
 };
 
